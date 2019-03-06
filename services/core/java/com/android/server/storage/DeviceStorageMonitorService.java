Neoteric-OS/android_frameworks_base

Fix notification shows wrong question
I think it should show "low_internal_storage_view_text"
 when storage is above 250M

Change-Id: I28319dd8746eee23b6bfb78980e5bd93746640e4
Signed-off-by: pengzhicai <pengzhicai@xiaomi.com>

services/core/java/com/android/server/storage/DeviceStorageMonitorService.java
@@ -98,6 +98,7 @@ public class DeviceStorageMonitorService extends SystemService {
     /** Map from storage volume UUID to internal state */
     private final ArrayMap<UUID, State> mStates = new ArrayMap<>();
 
+    private long mFreeMem = 0;
     /**
      * State for a specific storage volume, including the current "level" that
      * we've alerted the user and apps about.
@@ -203,6 +204,7 @@ public class DeviceStorageMonitorService extends SystemService {
             final long totalBytes = file.getTotalSpace();
             final long usableBytes = file.getUsableSpace();
 
+            mFreeMem = usableBytes;
             int oldLevel = state.level;
             int newLevel;
             if (mForceLevel != State.LEVEL_UNKNOWN) {
@@ -220,6 +222,7 @@ public class DeviceStorageMonitorService extends SystemService {
             } else {
                 newLevel = State.LEVEL_NORMAL;
             }
+
 
             // Log whenever we notice drastic storage changes
             if ((Math.abs(state.lastUsableBytes - usableBytes) > DEFAULT_LOG_DELTA_BYTES)
@@ -469,9 +472,10 @@ public class DeviceStorageMonitorService extends SystemService {
 
             final CharSequence details;
             if (StorageManager.UUID_DEFAULT.equals(uuid)) {
-                details = context.getText(isBootImageOnDisk()
-                        ? com.android.internal.R.string.low_internal_storage_view_text
-                        : com.android.internal.R.string.low_internal_storage_view_text_no_boot);
+                details = context.getText((!isBootImageOnDisk() 
+                        && usableBytes < BOOT_IMAGE_STORAGE_REQUIREMENT)
+                        ? com.android.internal.R.string.low_internal_storage_view_text_no_boot
+                        : com.android.internal.R.string.low_internal_storage_view_text);
             } else {
                 details = context.getText(
                         com.android.internal.R.string.low_internal_storage_view_text);

alphamalee/frameworks_base

Signed-off-by: mydongistiny <jaysonedson@gmail.com>

services/core/java/com/android/server/storage/DeviceStorageMonitorService.java
@@ -99,6 +99,7 @@ public class DeviceStorageMonitorService extends SystemService {
     /** Map from storage volume UUID to internal state */
     private final ArrayMap<UUID, State> mStates = new ArrayMap<>();
 
+    private long mFreeMem = 0;
     /**
      * State for a specific storage volume, including the current "level" that
      * we've alerted the user and apps about.
@@ -204,6 +205,7 @@ public class DeviceStorageMonitorService extends SystemService {
             final long totalBytes = file.getTotalSpace();
             final long usableBytes = file.getUsableSpace();
 
+            mFreeMem = usableBytes;
             int oldLevel = state.level;
             int newLevel;
             if (mForceLevel != State.LEVEL_UNKNOWN) {
@@ -470,9 +472,10 @@ public class DeviceStorageMonitorService extends SystemService {
 
             final CharSequence details;
             if (StorageManager.UUID_DEFAULT.equals(uuid)) {
-                details = context.getText(isBootImageOnDisk()
-                        ? com.android.internal.R.string.low_internal_storage_view_text
-                        : com.android.internal.R.string.low_internal_storage_view_text_no_boot);
+                details = context.getText((!isBootImageOnDisk()
+                        && mFreeMem < BOOT_IMAGE_STORAGE_REQUIREMENT)
+                        ? com.android.internal.R.string.low_internal_storage_view_text_no_boot
+                        : com.android.internal.R.string.low_internal_storage_view_text);
             } else {
                 details = context.getText(
                         com.android.internal.R.string.low_internal_storage_view_text);